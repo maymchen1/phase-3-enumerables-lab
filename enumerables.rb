3ce199e79d990b99f0f6ae3a8fa01f2d3b9077d0
May Chen <maymchen1@gmail.com>
2022-11-07 17:16:21 -0500

    commit tree ea4a8aa56b714e52927bb0cac92d31764fd28727

--- enumerables.rb
+++ enumerables.rb
@@ -21,7 +21,7 @@ end
 # where the heat level of the food is greater than 5
 def spiciest_foods(spicy_foods)
   spiciest_foods.select do |food|
-    food.[heat_level] > 5
+    food[:heat_level] > 5
   end 
 end
 
@@ -59,7 +59,6 @@ end
 def print_spiciest_foods(spicy_foods)
   spiciest = spiciest_foods(spicy_foods)
   print_spicy_foods(spiciest)
-  end 
 end
 
 # given an array of spicy foods, return an integer representing 
